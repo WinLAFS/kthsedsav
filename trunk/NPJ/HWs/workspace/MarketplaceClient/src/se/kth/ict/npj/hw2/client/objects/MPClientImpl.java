3bdf2c40599dc8275c24ad0774a9427a7e6b2b3e
shumanski <shumanski@9a21e158-ce10-11de-8d2e-8bf2cb4fc7ae>
2009-11-14 15:06:00 +0000

    MPCLIENT implementation starts

---
+++ trunk/NPJ/HWs/workspace/MarketplaceClient/src/se/kth/ict/npj/hw2/client/objects/MPClientImpl.java
@@ -0,0 +1,27 @@
+package se.kth.ict.npj.hw2.client.objects;
+
+import java.rmi.RemoteException;
+
+import se.kth.ict.npj.hw2.Item;
+
+public class MPClientImpl implements MPClientInterface {
+
+	@Override
+	public String getId() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void receiveItemSoldNotification(Item item) throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void receiveWishedItemNotification(Item item) throws RemoteException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
