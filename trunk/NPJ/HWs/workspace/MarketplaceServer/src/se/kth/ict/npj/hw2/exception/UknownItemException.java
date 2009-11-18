4e5ce87bf74597d95b6860200e7544cc3f15f94f
shumanski <shumanski@9a21e158-ce10-11de-8d2e-8bf2cb4fc7ae>
2009-11-18 13:19:54 +0000

    Exception changes

--- trunk/NPJ/HWs/workspace/MarketplaceServer/src/se/kth/ict/npj/hw2/exception/UknownItemException.java
+++ trunk/NPJ/HWs/workspace/MarketplaceServer/src/se/kth/ict/npj/hw2/exception/UknownItemException.java
@@ -2,7 +2,7 @@ package se.kth.ict.npj.hw2.exception;
 
 import java.rmi.RemoteException;
 
-public class UknownItemException extends RemoteException {
+public class UknownItemException extends Exception {
 
 	public UknownItemException() {
 		super();
