b4be0cbb440de1046aa3bda6db36a460b36b2334
shumanski <shumanski@9a21e158-ce10-11de-8d2e-8bf2cb4fc7ae>
2009-11-26 20:55:28 +0000

    Beautiful code

--- NPJ/project/workspace1/NicheCounter/src/counter/executors/CounterStateChangedExecutor.java
+++ NPJ/project/workspace1/NicheCounter/src/counter/executors/CounterStateChangedExecutor.java
@@ -10,51 +10,60 @@ import org.objectweb.fractal.api.control.LifeCycleController;
 
 import counter.actuators.CounterStatusActuator;
 import counter.events.ComponentOutOfSyncEvent;
-import counter.events.CounterChangedEvent;
 import counter.events.InformOutOfSyncEvent;
-import counter.interfaces.CounterResyncInterface;
-import counter.sensors.CounterStatusSensor;
-
+import counter.managers.ConfigurationManager;
 import dks.niche.fractal.FractalInterfaceNames;
 import dks.niche.fractal.interfaces.DeployActuatorsInterface;
-import dks.niche.fractal.interfaces.DeploySensorsInterface;
 import dks.niche.fractal.interfaces.EventHandlerInterface;
 import dks.niche.fractal.interfaces.InitInterface;
 import dks.niche.fractal.interfaces.MovableInterface;
 import dks.niche.fractal.interfaces.TriggerInterface;
-import dks.niche.ids.ComponentId;
 import dks.niche.ids.NicheId;
 import dks.niche.interfaces.NicheActuatorInterface;
 
+/**
+ * Executor handles {@link InformOutOfSyncEvent} triggered by
+ * {@link ConfigurationManager} and then triggers
+ * {@link ComponentOutOfSyncEvent} to {@link CounterStatusActuator} actuator.
+ * Executor also deploys actuators {@link CounterStatusActuator}
+ * 
+ */
 public class CounterStateChangedExecutor implements EventHandlerInterface,
-MovableInterface, BindingController, LifeCycleController, InitInterface {
+		MovableInterface, BindingController, LifeCycleController, InitInterface {
 
 	private Component myself;
 	private NicheActuatorInterface actuator;
-//	private DeploySensorsInterface deploySensor;
 	private DeployActuatorsInterface deployActuator;
 	private TriggerInterface trigger;
-	
-	
 
 	private boolean status;
 
-	@Override
+	/**
+	 * The method handles {@link InformOutOfSyncEvent}
+	 * 
+	 * @see dks.niche.fractal.interfaces.EventHandlerInterface#eventHandler(java.io.Serializable,
+	 *      int)
+	 */
 	public void eventHandler(Serializable event, int flag) {
 		if (event instanceof InformOutOfSyncEvent) {
-			int value = ((InformOutOfSyncEvent)event).getCounterNumber();
-			int lamport = ((InformOutOfSyncEvent)event).getLamport();
-			//ComponentId cid = ((CounterChangedEvent)event).getCid();
-//			System.out.println("[executor] InformOutOfSyncEvent received. Value: "+value);
+			int value = ((InformOutOfSyncEvent) event).getCounterNumber();
+			int lamport = ((InformOutOfSyncEvent) event).getLamport();
 			trigger.trigger(new ComponentOutOfSyncEvent(value, lamport));
 		}
 
 	}
 
+	/**
+	 * @see dks.niche.fractal.interfaces.MovableInterface#getAttributes()
+	 */
 	public Serializable[] getAttributes() {
 		return new Serializable[] {};
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.BindingController#bindFc(java.lang.String,
+	 *      java.lang.Object)
+	 */
 	public void bindFc(final String itfName, final Object itfValue)
 			throws NoSuchInterfaceException {
 		if (itfName.equals("component"))
@@ -65,20 +74,25 @@ MovableInterface, BindingController, LifeCycleController, InitInterface {
 		else if (itfName
 				.equals(FractalInterfaceNames.DEPLOY_ACTUATOR_CLIENT_INTERFACE))
 			deployActuator = (DeployActuatorsInterface) itfValue;
-		else if (itfName
-				.equals(FractalInterfaceNames.TRIGGER_CLIENT_INTERFACE))
+		else if (itfName.equals(FractalInterfaceNames.TRIGGER_CLIENT_INTERFACE))
 			trigger = (TriggerInterface) itfValue;
 		else
 			throw new NoSuchInterfaceException(itfName);
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.BindingController#listFc()
+	 */
 	public String[] listFc() {
 		return new String[] { FractalInterfaceNames.COMPONENT,
 				FractalInterfaceNames.ACTUATOR_CLIENT_INTERFACE,
 				FractalInterfaceNames.DEPLOY_ACTUATOR_CLIENT_INTERFACE,
-				FractalInterfaceNames.TRIGGER_CLIENT_INTERFACE};
+				FractalInterfaceNames.TRIGGER_CLIENT_INTERFACE };
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.BindingController#lookupFc(java.lang.String)
+	 */
 	public Object lookupFc(final String itfName)
 			throws NoSuchInterfaceException {
 		if (itfName.equals(FractalInterfaceNames.COMPONENT))
@@ -89,16 +103,15 @@ MovableInterface, BindingController, LifeCycleController, InitInterface {
 		else if (itfName
 				.equals(FractalInterfaceNames.DEPLOY_ACTUATOR_CLIENT_INTERFACE))
 			return deployActuator;
-		else if (itfName
-				.equals(FractalInterfaceNames.TRIGGER_CLIENT_INTERFACE))
+		else if (itfName.equals(FractalInterfaceNames.TRIGGER_CLIENT_INTERFACE))
 			return trigger;
-		// else if
-		// (itfName.equals(FractalInterfaceNames.TRIGGER_CLIENT_INTERFACE))
-		// return triggerInterface;
 		else
 			throw new NoSuchInterfaceException(itfName);
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.BindingController#unbindFc(java.lang.String)
+	 */
 	public void unbindFc(final String itfName) throws NoSuchInterfaceException {
 		if (itfName.equals(FractalInterfaceNames.COMPONENT))
 			myself = null;
@@ -108,60 +121,74 @@ MovableInterface, BindingController, LifeCycleController, InitInterface {
 		else if (itfName
 				.equals(FractalInterfaceNames.DEPLOY_ACTUATOR_CLIENT_INTERFACE))
 			deployActuator = null;
-		else if (itfName
-				.equals(FractalInterfaceNames.TRIGGER_CLIENT_INTERFACE))
+		else if (itfName.equals(FractalInterfaceNames.TRIGGER_CLIENT_INTERFACE))
 			trigger = null;
-		// else if
-		// (itfName.equals(FractalInterfaceNames.TRIGGER_CLIENT_INTERFACE))
-		// triggerInterface = null;
 
 		else
 			throw new NoSuchInterfaceException(itfName);
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.LifeCycleController#getFcState()
+	 */
 	public String getFcState() {
 		return status ? "STARTED" : "STOPPED";
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.LifeCycleController#startFc()
+	 */
 	public void startFc() throws IllegalLifeCycleException {
 
 		status = true;
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.LifeCycleController#stopFc()
+	 */
 	public void stopFc() throws IllegalLifeCycleException {
 		status = false;
 
 	}
 
-	@Override
+	/**
+	 * @see dks.niche.fractal.interfaces.InitInterface#init(java.io.Serializable[])
+	 */
 	public void init(Serializable[] parameters) {
-		initSmth() ;
+		initActuators();
 	}
 
-	@Override
+	/**
+	 * @see dks.niche.fractal.interfaces.InitInterface#init(dks.niche.interfaces.NicheActuatorInterface)
+	 */
 	public void init(NicheActuatorInterface actuator) {
-		initSmth() ;
+		initActuators();
 	}
 
-	@Override
+	/**
+	 * @see dks.niche.fractal.interfaces.InitInterface#initId(dks.niche.ids.NicheId)
+	 */
 	public void initId(NicheId id) {
-		initSmth() ;
+		initActuators();
 	}
 
-	@Override
+	/**
+	 * @see dks.niche.fractal.interfaces.InitInterface#reinit(java.io.Serializable[])
+	 */
 	public void reinit(Serializable[] parameters) {
-		initSmth() ;
+		initActuators();
 	}
 
-	private void initSmth() {
+	/**
+	 * The method deploys actuators {@link CounterStatusActuator}. Actuators are
+	 * sybscribed to {@link ComponentOutOfSyncEvent}
+	 */
+	private void initActuators() {
 		System.out.println("[executor]> Initialize");
 		Serializable[] actuatorParameters = new Serializable[2];
-
-//		deploySensor.deploySensor(CounterStatusSensor.class.getName(),
-//				CounterChangedEvent.class.getName(), sensorParameters, null,
-//				new String[] { "counterStatus" });
-		deployActuator.deployActuator(CounterStatusActuator.class.getName(), 
-				ComponentOutOfSyncEvent.class.getName(), actuatorParameters, new String[] { "counterResync" }, null);
+		deployActuator.deployActuator(CounterStatusActuator.class.getName(),
+				ComponentOutOfSyncEvent.class.getName(), actuatorParameters,
+				new String[] { "counterResync" }, null);
 	}
 
 }
