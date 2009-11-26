6c27baed74aa93e0049073ee69d616fd7a2b4e93
shumanski <shumanski@9a21e158-ce10-11de-8d2e-8bf2cb4fc7ae>
2009-11-26 20:41:13 +0000

    Beautiful code

--- NPJ/project/workspace1/NicheCounter/src/counter/frontend/FrontendComponent.java
+++ NPJ/project/workspace1/NicheCounter/src/counter/frontend/FrontendComponent.java
@@ -1,108 +1,121 @@
 package counter.frontend;
 
-import counter.interfaces.CounterInterface;
-import counter.interfaces.HelloAllInterface;
-import counter.interfaces.HelloAnyInterface;
-
 import org.objectweb.fractal.api.Component;
 import org.objectweb.fractal.api.NoSuchInterfaceException;
 import org.objectweb.fractal.api.control.BindingController;
 import org.objectweb.fractal.api.control.IllegalLifeCycleException;
 import org.objectweb.fractal.api.control.LifeCycleController;
 
-public class FrontendComponent implements BindingController, LifeCycleController {
+import counter.interfaces.CounterInterface;
+import counter.service.ServiceComponent;
 
-    // Client Interfaces
-    private HelloAnyInterface helloAny;
-    private HelloAllInterface helloAll;
-    private CounterInterface counter;
+/**
+ * The front end component of the application. It has a {@link CounterInterface}
+ * as a client interface and use it to make calls to {@link ServiceComponent} At
+ * startup {@link FrontendComponent}. creates {@link UserInterface}.
+ * 
+ */
+public class FrontendComponent implements BindingController,
+		LifeCycleController {
 
-    // The Niche reference to this component.
-    private Component myself;
+	// Client Interfaces
+	private CounterInterface counter;
 
-    // Is this component active or not?
-    private boolean status;
+	// The Niche reference to this component.
+	private Component myself;
 
-    // ///////////////////////////////////////////////////////////////
-    // ///////////////// Called from the user interface. /////////////
-    // ///////////////////////////////////////////////////////////////
+	// Is this component active or not?
+	private boolean status;
 
-    public synchronized void helloAny() {
-        helloAny.helloAny("Counter");
-    }
+	// ///////////////////////////////////////////////////////////////
+	// ///////////////// Called from the user interface. /////////////
+	// ///////////////////////////////////////////////////////////////
 
-    public synchronized void helloAll() {
-        helloAll.helloAll("Counter");
-    }
-    
-    public synchronized void increaseCounter(int a) {
-    	counter.inreaseCounter(a);
-    }
+	/**
+	 * The method calls increaseCounter method of the client {@link CounterInterface}
+	 * 
+	 * @param a
+	 */
+	public synchronized void increaseCounter(int a) {
+		counter.inreaseCounter(a);
+	}
 
-    // ////////////////////////////////////////////////////////////////////////
-    // ////////////////////////// Fractal Stuff ///////////////////////////////
-    // ////////////////////////////////////////////////////////////////////////
+	// ////////////////////////////////////////////////////////////////////////
+	// ////////////////////////// Fractal Stuff ///////////////////////////////
+	// ////////////////////////////////////////////////////////////////////////
 
-    public String[] listFc() {
-        return new String[] { "component", "helloAny", "helloAll", "counter" };
-    }
+	/**
+	 * @see org.objectweb.fractal.api.control.BindingController#listFc()
+	 */
+	public String[] listFc() {
+		return new String[] { "component", "counter" };
+	}
 
-    public Object lookupFc(final String itfName) throws NoSuchInterfaceException {
-        if (itfName.equals("helloAny")) {
-            return helloAny;
-        } else if (itfName.equals("helloAll")) {
-            return helloAll;
-        } else if (itfName.equals("counter")) {
-            return counter;
-        } else if (itfName.equals("component")) {
-            return myself;
-        } else {
-            throw new NoSuchInterfaceException(itfName);
-        }
-    }
+	/**
+	 * @see org.objectweb.fractal.api.control.BindingController#lookupFc(java.lang.String)
+	 */
+	public Object lookupFc(final String itfName)
+			throws NoSuchInterfaceException {
+		if (itfName.equals("counter")) {
+			return counter;
+		} else if (itfName.equals("component")) {
+			return myself;
+		} else {
+			throw new NoSuchInterfaceException(itfName);
+		}
+	}
 
-    public void bindFc(final String itfName, final Object itfValue) throws NoSuchInterfaceException {
-        System.out.println(">> >>Binding " + itfName + " : null ? " + (itfValue == null));
-        System.out.println(">> >>Value: " + (itfValue.toString()));
-    	if (itfName.equals("helloAny")) {
-            helloAny = (HelloAnyInterface) itfValue;
-        } else if (itfName.equals("helloAll")) {
-            helloAll = (HelloAllInterface) itfValue;
-        } else if (itfName.equals("component")) {
-            myself = (Component) itfValue;
-        } else if (itfName.equals("counter")) {
-            counter = (CounterInterface) itfValue;
-        } else {
-            throw new NoSuchInterfaceException(itfName);
-        }
-    }
+	/**
+	 * @see org.objectweb.fractal.api.control.BindingController#bindFc(java.lang.String, java.lang.Object)
+	 */
+	public void bindFc(final String itfName, final Object itfValue)
+			throws NoSuchInterfaceException {
+		System.out.println(">> >>Binding " + itfName + " : null ? "
+				+ (itfValue == null));
+		System.out.println(">> >>Value: " + (itfValue.toString()));
+		if (itfName.equals("component")) {
+			myself = (Component) itfValue;
+		} else if (itfName.equals("counter")) {
+			counter = (CounterInterface) itfValue;
+		} else {
+			throw new NoSuchInterfaceException(itfName);
+		}
+	}
 
-    public void unbindFc(final String itfName) throws NoSuchInterfaceException {
-        if (itfName.equals("helloAny")) {
-            helloAny = null;
-        } else if (itfName.equals("helloAll")) {
-            helloAll = null;
-        } else if (itfName.equals("component")) {
-            myself = null;
-        } else if (itfName.equals("counter")) {
-            counter = null;
-        } else {
-            throw new NoSuchInterfaceException(itfName);
-        }
-    }
+	/**
+	 * @see org.objectweb.fractal.api.control.BindingController#unbindFc(java.lang.String)
+	 */
+	public void unbindFc(final String itfName) throws NoSuchInterfaceException {
+		if (itfName.equals("component")) {
+			myself = null;
+		} else if (itfName.equals("counter")) {
+			counter = null;
+		} else {
+			throw new NoSuchInterfaceException(itfName);
+		}
+	}
 
-    public String getFcState() {
-        return status ? "STARTED" : "STOPPED";
-    }
+	/**
+	 * @see org.objectweb.fractal.api.control.LifeCycleController#getFcState()
+	 */
+	public String getFcState() {
+		return status ? "STARTED" : "STOPPED";
+	}
 
-    public void startFc() throws IllegalLifeCycleException {
-        // Create the GUI.
-        new UserInterface(this);
-        status = true;
-        System.err.println("Frontend component started.");
-    }
+	/**
+	 * @see org.objectweb.fractal.api.control.LifeCycleController#startFc()
+	 */
+	public void startFc() throws IllegalLifeCycleException {
+		// Create the GUI.
+		new UserInterface(this);
+		status = true;
+		System.err.println("Frontend component started.");
+	}
 
-    public void stopFc() throws IllegalLifeCycleException {
-        status = false;
-    }
+	/**
+	 * @see org.objectweb.fractal.api.control.LifeCycleController#stopFc()
+	 */
+	public void stopFc() throws IllegalLifeCycleException {
+		status = false;
+	}
 }
