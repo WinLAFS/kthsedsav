9569afc053a8f8a79628714b5fbb22fce09b3eb2
shumanski <shumanski@9a21e158-ce10-11de-8d2e-8bf2cb4fc7ae>
2009-11-26 21:15:49 +0000

    Beautiful code

--- trunk/NPJ/project/workspace1/NicheCounter/src/counter/actuators/CounterStatusActuator.java
+++ trunk/NPJ/project/workspace1/NicheCounter/src/counter/actuators/CounterStatusActuator.java
@@ -9,25 +9,25 @@ import org.objectweb.fractal.api.control.IllegalLifeCycleException;
 import org.objectweb.fractal.api.control.LifeCycleController;
 
 import counter.events.ComponentOutOfSyncEvent;
-import counter.events.CounterChangedEvent;
-import counter.events.SynchronizeCompononentEvent;
+import counter.executors.CounterStateChangedExecutor;
 import counter.interfaces.CounterResyncInterface;
-import counter.interfaces.CounterStatusInterface;
-import counter.interfaces.SynchronizeInterface;
+import counter.service.ServiceComponent;
 import dks.niche.fractal.interfaces.ActuatorInitInterface;
 import dks.niche.fractal.interfaces.EventHandlerInterface;
-import dks.niche.fractal.interfaces.SensorInitInterface;
-import dks.niche.fractal.interfaces.TriggerInterface;
 import dks.niche.ids.ComponentId;
 import dks.niche.ids.NicheId;
 import dks.niche.interfaces.NicheActuatorInterface;
-import dks.niche.interfaces.NicheAsynchronousInterface;
 
+/**
+ * The actuator to re-synchronize {@link ServiceComponent} components. Actuator
+ * receives {@link ComponentOutOfSyncEvent} and calls
+ * {@link CounterResyncInterface#reSynchronize(int, int)} method of the
+ * component
+ * 
+ */
 public class CounterStatusActuator implements ActuatorInitInterface,
 		BindingController, LifeCycleController, EventHandlerInterface {
 
-
-	// ///////////////
 	Component mySelf;
 	private boolean status;
 
@@ -35,9 +35,12 @@ public class CounterStatusActuator implements ActuatorInitInterface,
 	private NicheId myId;
 	private ComponentId myComponentId;
 	private NicheActuatorInterface myNicheActuatorInterface;
-	
+
 	private CounterResyncInterface counterResync;
 
+	/**
+	 * Default constructor
+	 */
 	public CounterStatusActuator() {
 		mySelf = null;
 	}
@@ -47,124 +50,157 @@ public class CounterStatusActuator implements ActuatorInitInterface,
 	// ///////////////////////////////
 	// //////////////////////////////////////////////////////////////////////////
 
-	// ////// InitInterface
+	/**
+	 * @param parameters
+	 */
 	public void init(Object[] parameters) {
-		// delta = (Integer)parameters[0];
-		// log("$$$$$$$$$$$$ LoadSensor: delta = " + delta);
 
 	}
 
+	/**
+	 * @see dks.niche.fractal.interfaces.InitInterface#init(dks.niche.interfaces.NicheActuatorInterface)
+	 */
 	public void init(NicheActuatorInterface actuator) {
 		myNicheActuatorInterface = actuator;
 	}
 
+	/**
+	 * Init method
+	 * 
+	 * @param id
+	 *            NicheId of the actuator
+	 */
 	public void initId(Object id) {
 		myId = (NicheId) id;
 	}
 
+	/**
+	 * @see dks.niche.fractal.interfaces.ActuatorInitInterface#initComponentId(dks.niche.ids.ComponentId)
+	 */
 	public void initComponentId(ComponentId cid) {
 		myComponentId = cid;
 
 	}
 
+	/**
+	 * @param applicationParameters
+	 */
 	public void reinit(Object[] applicationParameters) {
 	}
-
-//	public void informCounterValue(ComponentId cid, int value) {
-//		this.trigger.trigger(new CounterChangedEvent(cid, value));
-//		
-//	}
-
-	// //////////////////////////////////////////////////////////////////////////
-	// /////////////////////////// Attributes
-	// ///////////////////////////////////
-	// //////////////////////////////////////////////////////////////////////////
-
 
 	// //////////////////////////////////////////////////////////////////////////
 	// //////////////////////// Fractal Stuff
 	// ///////////////////////////////////
 	// //////////////////////////////////////////////////////////////////////////
 
+	/**
+	 * @see org.objectweb.fractal.api.control.BindingController#listFc()
+	 */
 	public String[] listFc() {
-
 		return new String[] { "component", "counterResync" };
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.BindingController#lookupFc(java.lang.String)
+	 */
 	public Object lookupFc(final String itfName)
 			throws NoSuchInterfaceException {
-
 		if (itfName.equals("component"))
 			return mySelf;
 		else if (itfName.equals("counterResync")) {
 			return counterResync;
-		}
-		else
+		} else
 			throw new NoSuchInterfaceException(itfName);
 
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.BindingController#bindFc(java.lang.String,
+	 *      java.lang.Object)
+	 */
 	public void bindFc(final String itfName, final Object itfValue)
 			throws NoSuchInterfaceException {
 		if (itfName.equals("component"))
 			mySelf = (Component) itfValue;
 		else if (itfName.equals("counterResync")) {
 			counterResync = (CounterResyncInterface) itfValue;
-		}
-		else
+		} else
 			throw new NoSuchInterfaceException(itfName);
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.BindingController#unbindFc(java.lang.String)
+	 */
 	public void unbindFc(final String itfName) throws NoSuchInterfaceException {
 		if (itfName.equals("component"))
 			mySelf = null;
 		else if (itfName.equals("counterResync")) {
 			counterResync = null;
-		}
-		else
+		} else
 			throw new NoSuchInterfaceException(itfName);
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.LifeCycleController#getFcState()
+	 */
 	public String getFcState() {
 
 		return status ? "STARTED" : "STOPPED";
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.LifeCycleController#startFc()
+	 */
 	public void startFc() throws IllegalLifeCycleException {
 		status = true;
 	}
 
+	/**
+	 * @see org.objectweb.fractal.api.control.LifeCycleController#stopFc()
+	 */
 	public void stopFc() throws IllegalLifeCycleException {
 		status = false;
 
 	}
 
-	@Override
+	/**
+	 * @see dks.niche.fractal.interfaces.InitInterface#init(java.io.Serializable[])
+	 */
 	public void init(Serializable[] parameters) {
-		
+
 	}
 
-	@Override
+	/**
+	 * @see dks.niche.fractal.interfaces.InitInterface#initId(dks.niche.ids.NicheId)
+	 */
 	public void initId(NicheId id) {
-		
+
 	}
 
-	@Override
+	/**
+	 * @see dks.niche.fractal.interfaces.InitInterface#reinit(java.io.Serializable[])
+	 */
 	public void reinit(Serializable[] parameters) {
-		
+
 	}
 
-	@Override
+	/**
+	 * The method handles {@link ComponentOutOfSyncEvent} event from
+	 * {@link CounterStateChangedExecutor}
+	 * 
+	 * @see dks.niche.fractal.interfaces.EventHandlerInterface#eventHandler(java.io.Serializable,
+	 *      int)
+	 */
 	public void eventHandler(Serializable event, int flag) {
 		if (event instanceof ComponentOutOfSyncEvent) {
 			ComponentOutOfSyncEvent evt = (ComponentOutOfSyncEvent) event;
-//			System.out.println("[actuator]> ComponentOutOfSyncEvent received. Value: " + evt.getCounterNumber());
-			counterResync.reSynchronize(evt.getCounterNumber(), evt.getLamport());
+			counterResync.reSynchronize(evt.getCounterNumber(), evt
+					.getLamport());
+		} else {
+			System.out.println("[actuator]> uknown event: "
+					+ event.getClass().getName());
 		}
-		else {
-			System.out.println("[actuator]> uknown event: " + event.getClass().getName());
-		}
-		
+
 	}
 
 }
