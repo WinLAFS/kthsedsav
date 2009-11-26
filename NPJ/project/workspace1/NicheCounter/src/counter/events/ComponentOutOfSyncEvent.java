ba46c1635d2b532e8a03d92b9ef84074916627f6
shumanski <shumanski@9a21e158-ce10-11de-8d2e-8bf2cb4fc7ae>
2009-11-26 22:20:21 +0000

    Beautiful code

--- NPJ/project/workspace1/NicheCounter/src/counter/events/ComponentOutOfSyncEvent.java
+++ NPJ/project/workspace1/NicheCounter/src/counter/events/ComponentOutOfSyncEvent.java
@@ -2,36 +2,45 @@ package counter.events;
 
 import java.io.Serializable;
 
+import counter.aggregators.ServiceSupervisor;
+import counter.executors.CounterStateChangedExecutor;
+import counter.managers.ConfigurationManager;
 import dks.arch.Event;
-import dks.niche.ids.ComponentId;
 
+/**
+ * The event is used to make a notification that some components are not
+ * synchronized. Is sent by {@link ServiceSupervisor} to
+ * {@link ConfigurationManager} and from {@link ConfigurationManager} to
+ * {@link CounterStateChangedExecutor}
+ * 
+ */
 public class ComponentOutOfSyncEvent extends Event implements Serializable {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private int counterNumber;
-	private int lamport;
-	
-	public ComponentOutOfSyncEvent(int counterNumber, int lamport){
+	private int roundId;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param counterNumber
+	 *            Maximum number of the counter
+	 * @param roundId
+	 *            Id of the round when counter number was updated
+	 */
+	public ComponentOutOfSyncEvent(int counterNumber, int roundId) {
 		this.counterNumber = counterNumber;
-		this.lamport = lamport;
+		this.roundId = roundId;
 	}
-	
-	
 
 	public int getLamport() {
-		return lamport;
+		return roundId;
 	}
-
-
 
 	public void setLamport(int lamport) {
-		this.lamport = lamport;
+		this.roundId = lamport;
 	}
-
-
 
 	public int getCounterNumber() {
 		return counterNumber;
--- NPJ/project/workspace1/NicheCounter/src/counter/events/CounterChangedEvent.java
+++ NPJ/project/workspace1/NicheCounter/src/counter/events/CounterChangedEvent.java
@@ -2,50 +2,54 @@ package counter.events;
 
 import java.io.Serializable;
 
+import counter.sensors.CounterStatusSensor;
+import counter.watchers.CounterChangedWatcher;
 import dks.arch.Event;
 import dks.niche.ids.ComponentId;
 
-
+/**
+ * The event is by {@link CounterStatusSensor} to {@link CounterChangedWatcher}
+ * to inform about change of the counter of a component
+ * 
+ */
 public class CounterChangedEvent extends Event implements Serializable {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private ComponentId cid;
 	private int counterNumber;
-	private int lamport;
-	
-	public CounterChangedEvent(ComponentId cid, int counterNumber, int lamport){
+	private int roundId;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param cid
+	 *            Id of the component that changed it's counter
+	 * @param counterNumber
+	 *            Number of the counter
+	 * @param roundId
+	 *            Id of the round
+	 */
+	public CounterChangedEvent(ComponentId cid, int counterNumber, int roundId) {
 		this.counterNumber = counterNumber;
 		this.cid = cid;
-		this.lamport = lamport;
+		this.roundId = roundId;
 	}
 
-	
-	
 	public int getLamport() {
-		return lamport;
+		return roundId;
 	}
-
-
 
 	public void setLamport(int lamport) {
-		this.lamport = lamport;
+		this.roundId = lamport;
 	}
-
-
 
 	public int getCounterNumber() {
 		return counterNumber;
 	}
-	
+
 	public ComponentId getCid() {
 		return cid;
 	}
-	
-//
-//	public ComponentId getSource() {
-//		return source;
-//	}
+
 }
