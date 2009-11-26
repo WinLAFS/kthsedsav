33f20d679e453ed3629778063241a040b234f197
shumanski <shumanski@9a21e158-ce10-11de-8d2e-8bf2cb4fc7ae>
2009-11-26 20:41:13 +0000

    Beautiful code

--- trunk/NPJ/project/workspace1/NicheCounter/src/counter/frontend/UserInterface.java
+++ trunk/NPJ/project/workspace1/NicheCounter/src/counter/frontend/UserInterface.java
@@ -10,103 +10,109 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * The class draws GUI and invokes {@link FrontendComponent} to increase counter
+ * 
+ */
 public class UserInterface extends JPanel {
-    private FrontendComponent myAI = null;
+	private FrontendComponent myAI = null;
 
-    /* UI elements */
-    JButton helloAnyButton, helloAllButton;
-    JLabel labelCounter;
-    
-    int correctCount = 0;
-    Random generator = new Random();
+	/* UI elements */
+	JButton countButton, count100Button;
+	JLabel labelCounter;
 
-    public UserInterface(FrontendComponent applicationInterface) {
-        myAI = applicationInterface;
-        createGUI();
-    }
+	int correctCount = 0;
+	Random generator = new Random();
 
-    private void createGUI() {
-        helloAnyButton = new JButton("Increase");
-        helloAnyButton.setVerticalTextPosition(AbstractButton.CENTER);
-        helloAnyButton.setHorizontalTextPosition(AbstractButton.LEADING);
-        // helloAnyButton.setMnemonic(KeyEvent.VK_D);
-        // helloAnyButton.setActionCommand("disable");
+	/**
+	 * Constructor
+	 * 
+	 * @param applicationInterface
+	 *            {@link FrontendComponent} objects which is linked with the GUI
+	 */
+	public UserInterface(FrontendComponent applicationInterface) {
+		myAI = applicationInterface;
+		createGUI();
+	}
 
-        //helloAllButton = new JButton("HelloAll");
-        helloAllButton = new JButton("Increase 100");
-        helloAllButton.setVerticalTextPosition(AbstractButton.CENTER);
-        helloAllButton.setHorizontalTextPosition(AbstractButton.LEADING);
-        // helloAllButton.setMnemonic(KeyEvent.VK_M);
+	/**
+	 * The method creates a frame with 2 buttons and label
+	 */
+	private void createGUI() {
+		countButton = new JButton("Increase");
+		countButton.setVerticalTextPosition(AbstractButton.CENTER);
+		countButton.setHorizontalTextPosition(AbstractButton.LEADING);
 
-        // Listen for actions on buttons 1 and 3.
-        helloAnyButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                helloAny();
-            }
-        });
-        helloAllButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                helloAll();
-            }
-        });
-        
-        labelCounter = new JLabel("");
-        labelCounter.setText("0");
-        
-        add(helloAnyButton);
-        add(helloAllButton);
-        add(labelCounter);
+		count100Button = new JButton("Increase 100");
+		count100Button.setVerticalTextPosition(AbstractButton.CENTER);
+		count100Button.setHorizontalTextPosition(AbstractButton.LEADING);
+		// helloAllButton.setMnemonic(KeyEvent.VK_M);
 
-        // Create and set up the window.
-        JFrame frame = new JFrame("Counter");
-        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Listen for actions on buttons 1 and 3.
+		countButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				count();
+			}
+		});
+		count100Button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				count100();
+			}
+		});
 
-        // Create and set up the content pane.
-        setOpaque(true); // content panes must be opaque
-        frame.setContentPane(this);
+		labelCounter = new JLabel("");
+		labelCounter.setText("0");
 
-        // Display the window.
-        frame.pack();
-        frame.setVisible(true);
-        frame.setSize(340, 70);
-    }
+		add(countButton);
+		add(count100Button);
+		add(labelCounter);
 
-    private void helloAny() {
-    	System.out.println("[ui]> Invoking Counter");
-    	int roundId = generator.nextInt();
+		// Create and set up the window.
+		JFrame frame = new JFrame("Counter");
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Create and set up the content pane.
+		setOpaque(true); // content panes must be opaque
+		frame.setContentPane(this);
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+		frame.setSize(340, 70);
+	}
+
+	/**
+	 * Method creates a single increase counter call to
+	 * {@link FrontendComponent}
+	 */
+	private void count() {
+		System.out.println("[ui]> Invoking Counter");
+		int roundId = generator.nextInt();
 		correctCount++;
 		myAI.increaseCounter(roundId);
-		labelCounter.setText(correctCount+"");
-    }
+		labelCounter.setText(correctCount + "");
+	}
 
-    private void helloAll() {//TODO
-        System.out.println("[ui]> Invoking Counter");
-        //myAI.helloAll();
-        
+	/**
+	 * Method makes 100 increase counter calls to {@link FrontendComponent}.
+	 * Calls are made witha small delay.
+	 */
+	private void count100() {
+		System.out.println("[ui]> Invoking Counter 100");
 
-//        myAI.increaseCounter("NICHE");
-//        for (int i = 0; i < 100; i++) {
-//        	myAI.increaseCounter("NICHE");
-//        }
-
-        for (int i = 0; i < 100; i++) {
-        int roundId = generator.nextInt();
-		correctCount++;
-		myAI.increaseCounter(roundId);
-		labelCounter.setText(correctCount+"");
-	        try {
+		for (int i = 0; i < 100; i++) {
+			int roundId = generator.nextInt();
+			correctCount++;
+			myAI.increaseCounter(roundId);
+			labelCounter.setText(correctCount + "");
+			try {
 				Thread.sleep(50);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				System.err.println("[ui]> InterruptedException in GUI");
 			}
-        }
-
-//        for (int i = 0; i < 100; i++) {
-//        	myAI.increaseCounter("NICHE");
-//        }
-    }
+		}
+	}
 }
