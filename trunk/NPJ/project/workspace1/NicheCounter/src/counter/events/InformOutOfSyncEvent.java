e063dbec2e0c9930290757ad415fa42e6942fe54
shumanski <shumanski@9a21e158-ce10-11de-8d2e-8bf2cb4fc7ae>
2009-11-26 22:20:21 +0000

    Beautiful code

--- trunk/NPJ/project/workspace1/NicheCounter/src/counter/events/InformOutOfSyncEvent.java
+++ trunk/NPJ/project/workspace1/NicheCounter/src/counter/events/InformOutOfSyncEvent.java
@@ -2,35 +2,44 @@ package counter.events;
 
 import java.io.Serializable;
 
+import counter.executors.CounterStateChangedExecutor;
+import counter.managers.ConfigurationManager;
+
 import dks.arch.Event;
 
+/**
+ * The event is sent by {@link ConfigurationManager} to
+ * {@link CounterStateChangedExecutor} to tell executor that some node is not
+ * synchronized.
+ * 
+ */
 public class InformOutOfSyncEvent extends Event implements Serializable {
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private int counterNumber;
-	private int lamport;
-	
-	public InformOutOfSyncEvent(int counterNumber, int lamport){
+	private int roundId;
+
+	/**
+	 * Counter
+	 * 
+	 * @param counter
+	 *            Number Current maximum counter number
+	 * @param roundId
+	 *            Id of the round when max counter was set
+	 */
+	public InformOutOfSyncEvent(int counterNumber, int roundId) {
 		this.counterNumber = counterNumber;
-		this.lamport = lamport;
+		this.roundId = roundId;
 	}
 
-	
-	
 	public int getLamport() {
-		return lamport;
+		return roundId;
 	}
 
-
-
-	public void setLamport(int lamport) {
-		this.lamport = lamport;
+	public void setLamport(int roundId) {
+		this.roundId = roundId;
 	}
-
-
 
 	public int getCounterNumber() {
 		return counterNumber;
