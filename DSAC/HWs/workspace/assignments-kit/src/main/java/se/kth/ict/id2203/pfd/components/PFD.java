e5c497348f040e806876edce5bab3f279eb2c084
b.trigonakis <b.trigonakis@9a21e158-ce10-11de-8d2e-8bf2cb4fc7ae>
2010-02-08 16:16:46 +0000

    pfd ready

--- DSAC/HWs/workspace/assignments-kit/src/main/java/se/kth/ict/id2203/pfd/components/PFD.java
+++ DSAC/HWs/workspace/assignments-kit/src/main/java/se/kth/ict/id2203/pfd/components/PFD.java
@@ -6,13 +6,14 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import se.kth.ict.id2203.application.Application0;
 import se.kth.ict.id2203.application.Application0Init;
-import se.kth.ict.id2203.flp2p.FairLossPointToPointLink;
 import se.kth.ict.id2203.pfd.events.CheckTimeoutEvent;
+import se.kth.ict.id2203.pfd.events.CrashEvent;
 import se.kth.ict.id2203.pfd.events.HeartbeatMessage;
 import se.kth.ict.id2203.pfd.events.HeartbeatTimeoutEvent;
+import se.kth.ict.id2203.pfd.ports.PerfectFailureDetector;
 import se.kth.ict.id2203.pp2p.PerfectPointToPointLink;
+import se.kth.ict.id2203.pp2p.Pp2pSend;
 import se.sics.kompics.ComponentDefinition;
 import se.sics.kompics.Handler;
 import se.sics.kompics.Positive;
@@ -28,14 +29,14 @@ public class PFD extends ComponentDefinition {
 
 	private Set<Address> neighborSet;
 	private Set<Address> aliveSet;
-	private Set<Address> detectedSet = new HashSet<Address>();
+	private Set<Address> detectedSet;
 	private Address self;
 
 	Positive<PerfectPointToPointLink> pp2p = positive(PerfectPointToPointLink.class);
-	Positive<FairLossPointToPointLink> flp2p = positive(FairLossPointToPointLink.class);
-
+	Positive<PerfectFailureDetector> pfdPort = positive(PerfectFailureDetector.class);
+	
 	private static final Logger logger = LoggerFactory
-			.getLogger(Application0.class);
+			.getLogger(PFD.class);
 
 	// private Positive<FailureDetectorPort> fpPositive =
 	// positive(FailureDetectorPort.class);
@@ -57,50 +58,70 @@ public class PFD extends ComponentDefinition {
 			logger.debug("Sending hb to all alive to neighboors");
 			String message = "heartbeat";
 			
+			//8 - 10
 			for (Address neighbor : neighborSet) {
 				logger.info("Sending hb message {} to {}", message, neighbor);
-				trigger(new HeartbeatMessage(self), pp2p);
+				HeartbeatMessage hbMessage = new HeartbeatMessage(self);
+				trigger(new Pp2pSend(neighbor, hbMessage), pp2p);
 			}
 			
+			//11
+			ScheduleTimeout st = new ScheduleTimeout(Gamma);
+			st.setTimeoutEvent(new HeartbeatTimeoutEvent(st));
+			trigger(st, timer);
 		}
 	};
 
 	Handler<CheckTimeoutEvent> ctHandler = new Handler<CheckTimeoutEvent>() {
 
 		public void handle(CheckTimeoutEvent arg0) {
-			 // scheduling a timeout
+			 //14 - 21
+			logger.info("Checking for dead neighbours");
+			for (Address neighbour : neighborSet) {
+				if ((!aliveSet.contains(neighbour)) && (!detectedSet.contains(neighbour))) {
+					detectedSet.add(neighbour);
+					trigger(new CrashEvent(neighbour), pfdPort);
+					logger.info("DEAD : : " + neighbour.toString());
+				}
+			}
 			
-			logger.debug("ok, it works!!");
+			aliveSet = new HashSet<Address>();
+			ScheduleTimeout st2 = new ScheduleTimeout(Gamma + Delta);
+			st2.setTimeoutEvent(new CheckTimeoutEvent(st2));
+			trigger(st2, timer);
 		}
 	};
 
 	Handler<Application0Init> handleInit = new Handler<Application0Init>() {
 		public void handle(Application0Init event) {
+			//2 - 5
+			logger.info("Algorithm Start running");
+			detectedSet = new HashSet<Address>();
 			neighborSet = event.getNeighborSet();
 			aliveSet = neighborSet;
 			self = event.getSelf();
-		}
-	};
-
-	Handler<Start> handleStart = new Handler<Start>() {
-		public void handle(Start event) {
 			ScheduleTimeout st = new ScheduleTimeout(Gamma);
 			st.setTimeoutEvent(new HeartbeatTimeoutEvent(st));
 			trigger(st, timer);
 			ScheduleTimeout st2 = new ScheduleTimeout(Gamma + Delta);
 			st2.setTimeoutEvent(new CheckTimeoutEvent(st2));
 			trigger(st2, timer);
-			logger.debug("p1 started!");
+		}
+	};
+
+	Handler<Start> handleStart = new Handler<Start>() {
+		public void handle(Start event) {
 		}
 	};
 
 	Handler<HeartbeatMessage> handlePp2pHeartbeatMessage = new Handler<HeartbeatMessage>() {
 		public void handle(HeartbeatMessage event) {
+			//24
 			logger.info("Received hb message from {}", event.getSource().toString());
 			aliveSet.add(event.getSource());
 		}
 	};
-
+	
 	private void doShutdown() { // TODO close when application is closing
 		System.out.close();
 		System.err.close();
