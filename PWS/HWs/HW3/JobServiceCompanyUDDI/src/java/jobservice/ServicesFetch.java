a4732d2e4d0a1451c799b2ddc74c7328c50c010e
b.trigonakis <b.trigonakis@9a21e158-ce10-11de-8d2e-8bf2cb4fc7ae>
2010-02-20 19:31:54 +0000

    recruiter service

--- PWS/HWs/HW3/JobServiceCompanyUDDI/src/java/jobservice/ServicesFetch.java
+++ PWS/HWs/HW3/JobServiceCompanyUDDI/src/java/jobservice/ServicesFetch.java
@@ -131,6 +131,48 @@ public class ServicesFetch {
         return ret;
     }
 
+    public String fetchEmploymentOfficeService() {
+        String ret = null;
+        try {
+            // Define find qualifiers and name patterns
+            Collection<String> findQualifiers = new ArrayList<String>();
+            findQualifiers.add(FindQualifier.SORT_BY_NAME_DESC);
+            Collection<String> namePatterns = new ArrayList<String>();
+            // qString refers to the organization name we are looking for
+            namePatterns.add("%" + "EmploymentOffice" + "%");
+            // Find orgs with names that matches qString
+            BulkResponse response = bqm.findOrganizations(findQualifiers, namePatterns, null, null, null, null);
+
+            //Iterate over discovered organizations and collect their service binding
+            Collection orgs = response.getCollection();
+            Iterator orgIter = orgs.iterator();
+            while (orgIter.hasNext()) {
+                Organization org = (Organization) orgIter.next();
+                System.out.println("Found org: " + org.getDescription() + "\n\t with services:");
+                Collection services = org.getServices();
+                Iterator svcIter = services.iterator();
+                while (svcIter.hasNext()) {
+                    Service svc = (Service) svcIter.next();
+                    Collection serviceBindings = svc.getServiceBindings();
+                    Iterator sbIter = serviceBindings.iterator();
+                    while (sbIter.hasNext()) {
+                        ServiceBinding sb = (ServiceBinding) sbIter.next();
+                        System.out.println("    : " + sb.getAccessURI());
+                        ret = sb.getAccessURI();
+//                        System.out.println("        : " +
+
+                    }
+
+                }
+            }
+            return ret;
+
+        } catch (JAXRException ex) {
+            Logger.getLogger(ServicesFetch.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ret;
+    }
+
     public static void main(String[] args) {
         ServicesFetch sf = new ServicesFetch();
         sf.fetchUniversityService();
