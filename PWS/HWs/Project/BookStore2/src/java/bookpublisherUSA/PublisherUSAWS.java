5e1bd6c1599b5408416a9e7e81df18ea89255e55
b.trigonakis <b.trigonakis@9a21e158-ce10-11de-8d2e-8bf2cb4fc7ae>
2010-03-21 18:59:30 +0000

    new methods

--- PWS/HWs/Project/BookStore2/src/java/bookpublisherUSA/PublisherUSAWS.java
+++ PWS/HWs/Project/BookStore2/src/java/bookpublisherUSA/PublisherUSAWS.java
@@ -5,14 +5,24 @@
 
 package bookpublisherUSA;
 
+import bookpublisher.BooksDB.BooksDB;
+import bookpublisher.PublisherWS;
 import bookpublisher.objects.Book;
 import bookpublisher.objects.CreditCard;
 import bookpublisher.objects.Invoice;
 import bookpublisher.objects.Location;
 import bookpublisher.objects.SellReturnObj;
 import bookpublisherUSA.BoksDB.BooksUSADB;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.jws.WebService;
 
 /**
@@ -67,10 +77,82 @@ public class PublisherUSAWS {
          return sro;
     }
 
-     public boolean purchaseCancel(String bookISBN, CreditCard card){
+      public void purchaseCancel(String bookISBN, CreditCard card) {
 
-         //TODO add logic
+          try {
+            FileOutputStream fout = new FileOutputStream("order");
+            // Print a line of text
+            // Close our output stream
+            new PrintStream(fout).println("");
+            fout.close();
 
-         return true;
-     }
+        } catch (IOException ex) {
+            Logger.getLogger(PublisherWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void orderBookPurchase(String bookISBN, CreditCard card, Location location) {
+        try {
+            //TODO
+            FileOutputStream fout = new FileOutputStream("order");
+            // Print a line of text
+
+            String txt = bookISBN;
+            txt += "|" + card.getHolderName();
+            ArrayList<Book> books = BooksUSADB.getInstance().getBooks("", "", bookISBN);
+            Book book = books.get(0);
+            txt += "|" + book.getAuthor();
+            txt += "|" + book.getTitle();
+            txt += "|" + book.getPrice();
+            txt += "|" + location.getAddress();
+            txt += "|" + location.getCity();
+            txt += "|" + location.getCountry();
+            txt += "|" + location.getPostCode();
+            new PrintStream(fout).println(txt);
+            // Close our output stream
+            fout.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(PublisherWS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public SellReturnObj bookOrderDone() {
+        try {
+
+            // Open an input stream
+            FileInputStream fin = new FileInputStream("order");
+            // Close our input stream
+            SellReturnObj sro = new SellReturnObj();
+
+            String txt = new DataInputStream(fin).readLine();
+            StringTokenizer st = new StringTokenizer(txt, "|");
+
+            Book book = new Book();
+            String bookISBN = st.nextToken();
+            book.setISBN(bookISBN);
+            Invoice invoice = new Invoice();
+            invoice.setBookISBN(bookISBN);
+            invoice.setBuyer(st.nextToken());
+            sro.setInvoice(invoice);
+            book.setAuthor(st.nextToken());
+            book.setTitle(st.nextToken());
+            book.setPrice(new Double(st.nextToken()));
+            sro.setBookInfo(book);
+            Location location = new Location();
+            location.setAddress(st.nextToken());
+            location.setCity(st.nextToken());
+            location.setCountry(st.nextToken());
+            location.setPostCode(st.nextToken());
+            sro.setLocation(location);
+
+            fin.close();
+
+            return sro;
+        } catch (IOException ex) {
+            Logger.getLogger(PublisherUSAWS.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
 }
