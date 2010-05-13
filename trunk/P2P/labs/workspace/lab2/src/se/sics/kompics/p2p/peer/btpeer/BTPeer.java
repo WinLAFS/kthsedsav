79d88db1d2dad09db81f8a707f8db3960ef04cb4
b.trigonakis <b.trigonakis@9a21e158-ce10-11de-8d2e-8bf2cb4fc7ae>
2010-05-13 15:24:55 +0000

    lab 2 done

--- trunk/P2P/labs/workspace/lab2/src/se/sics/kompics/p2p/peer/btpeer/BTPeer.java
+++ trunk/P2P/labs/workspace/lab2/src/se/sics/kompics/p2p/peer/btpeer/BTPeer.java
@@ -1,5 +1,7 @@
 package se.sics.kompics.p2p.peer.btpeer;
 
+import java.util.ArrayList;
+
 import se.sics.kompics.ComponentDefinition;
 import se.sics.kompics.Handler;
 import se.sics.kompics.Negative;
@@ -9,14 +11,17 @@ import se.sics.kompics.timer.Timer;
 import se.sics.kompics.p2p.peer.CloseConnection;
 import se.sics.kompics.p2p.peer.DataMessage;
 import se.sics.kompics.p2p.peer.DownloadRequest;
+import se.sics.kompics.p2p.peer.GetUploaderRequest;
 import se.sics.kompics.p2p.peer.GetUploaderResponse;
 import se.sics.kompics.p2p.peer.HandshakeRequest;
 import se.sics.kompics.p2p.peer.HandshakeResponse;
+import se.sics.kompics.p2p.peer.HandshakeStatus;
 import se.sics.kompics.p2p.peer.MessagePort;
 import se.sics.kompics.p2p.peer.PeerAddress;
 import se.sics.kompics.p2p.peer.PeerPort;
 import se.sics.kompics.p2p.peer.CompleteUploaders;
 import se.sics.kompics.p2p.peer.RegisterPeer;
+import se.sics.kompics.p2p.peer.UpdateBuffer;
 import se.sics.kompics.p2p.simulator.launch.PeerType;
 import se.sics.kompics.p2p.simulator.snapshot.Snapshot;
 
@@ -34,6 +39,10 @@ public final class BTPeer extends ComponentDefinition {
 	private int pieceSize;
 	
     private Boolean[] buffer;
+    private boolean isSeed;
+    private int numOfUploaders = 0;
+    private int numOfDownloaders = 0;
+    private ArrayList<PeerAddress> myUploaders = new ArrayList<PeerAddress>();
 	
 //-------------------------------------------------------------------
 	public BTPeer() {
@@ -69,11 +78,17 @@ public final class BTPeer extends ComponentDefinition {
 			Snapshot.addPeer(peerSelf, numOfPieces, peerType);
 			
 			trigger(new RegisterPeer(peerSelf, tracker, peerType), network);
-			
+
+			isSeed = true;
 			if (peerType != PeerType.SEED) {
-				SchedulePeriodicTimeout spt = new SchedulePeriodicTimeout(5000, 5000);
+				isSeed = false;
+				SchedulePeriodicTimeout spt = new SchedulePeriodicTimeout(5000, 5000); //check every 5s if you are a seed
 				spt.setTimeoutEvent(new CompleteUploaders(spt));
 				trigger(spt, timer);
+			}
+			
+			for (int i = 0; i < numOfPieces; i++) {
+				buffer[i] = isSeed;
 			}
 		}
 	};
@@ -81,30 +96,79 @@ public final class BTPeer extends ComponentDefinition {
 //-------------------------------------------------------------------
 	Handler<CompleteUploaders> handleCompleteUploaders = new Handler<CompleteUploaders>() {
 		public void handle(CompleteUploaders event) {
+			//here you check if you became a seed..
+			//step 5
+			if (!isSeed) {
+				isSeed = true;
+				for (boolean piece : buffer) {
+					isSeed = isSeed && piece;
+					if (!isSeed)
+						break;
+				}
+				
+				if (!isSeed) {
+					int freeDLSlots = indegree - numOfUploaders ;
+					if (freeDLSlots > 0) {
+						for (int i = 1; i <= freeDLSlots; i++) {
+							trigger(new GetUploaderRequest(peerSelf, tracker), network);
+						}
+					}
+				}
+			}
+			
+			
+			
 		}
 	};
 
 //-------------------------------------------------------------------
 	Handler<GetUploaderResponse> handleGetUploaderResponse = new Handler<GetUploaderResponse>() {
 		public void handle(GetUploaderResponse event) {
+			PeerAddress uploader = event.getUploader();
+			int pieceIndex = event.getPiece();
+			
+			//TODO update the list somewhere
+			if (!myUploaders.contains(uploader)) {
+				trigger(new HandshakeRequest(peerSelf, uploader, pieceIndex), network);
+			}
+			
 		}
 	};
 	
 //-------------------------------------------------------------------
 	Handler<HandshakeRequest> handleHandshakeRequest = new Handler<HandshakeRequest>() {
 		public void handle(HandshakeRequest event) {
+			PeerAddress downloader = event.getPeerSource();
+			int pieceIndex = event.getPiece();
+
+			if (outdegree - numOfDownloaders > 0) {
+				trigger(new HandshakeResponse(peerSelf, downloader, pieceIndex, HandshakeStatus.ACCEPT), network);
+			}
+			else {
+				trigger(new HandshakeResponse(peerSelf, downloader, pieceIndex, HandshakeStatus.REJECT), network);
+			}
 		}
 	};
 
 //-------------------------------------------------------------------
 	Handler<HandshakeResponse> handleHandshakeResponse = new Handler<HandshakeResponse>() {
 		public void handle(HandshakeResponse event) {
+			if (event.getHandshakeStatus() == HandshakeStatus.ACCEPT) {
+				PeerAddress uploader = event.getPeerSource();
+				
+				numOfUploaders++;
+				myUploaders.add(uploader);
+				
+				trigger(new DownloadRequest(peerSelf, uploader, event.getPiece()), network);
+			}
 		}
 	};
 
 //-------------------------------------------------------------------
 	Handler<DownloadRequest> handleDownloadRequest = new Handler<DownloadRequest>() {
 		public void handle(DownloadRequest event) {
+			trigger(new DataMessage(peerSelf, event.getPeerSource(), event.getPiece(), pieceSize), network);
+			numOfDownloaders++;
 		}
 	};
 
@@ -112,13 +176,21 @@ public final class BTPeer extends ComponentDefinition {
 	Handler<DataMessage> handleRecvDataMessage = new Handler<DataMessage>() {
 		public void handle(DataMessage event) {
 			int piece = event.getPiece();
+			buffer[piece] = true;
 			Snapshot.addPiece(peerSelf, piece);
+			trigger(new UpdateBuffer(peerSelf, tracker, piece), network);
+			
+			numOfUploaders--;
+			myUploaders.remove(event.getPeerSource());
+			
+			trigger(new CloseConnection(peerSelf, event.getPeerSource()), network);
 		}
 	};
 
 //-------------------------------------------------------------------
 	Handler<CloseConnection> handleCloseConnection = new Handler<CloseConnection>() {
 		public void handle(CloseConnection event) {
+			numOfDownloaders--;
 		}
 	};
 }
--- trunk/P2P/labs/workspace/lab2/src/se/sics/kompics/p2p/peer/tracker/Tracker.java
+++ trunk/P2P/labs/workspace/lab2/src/se/sics/kompics/p2p/peer/tracker/Tracker.java
@@ -1,6 +1,12 @@
 package se.sics.kompics.p2p.peer.tracker;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 import se.sics.kompics.ComponentDefinition;
 import se.sics.kompics.Handler;
@@ -8,6 +14,7 @@ import se.sics.kompics.Negative;
 import se.sics.kompics.Positive;
 import se.sics.kompics.timer.Timer;
 import se.sics.kompics.p2p.peer.GetUploaderRequest;
+import se.sics.kompics.p2p.peer.GetUploaderResponse;
 import se.sics.kompics.p2p.peer.MessagePort;
 import se.sics.kompics.p2p.peer.PeerAddress;
 import se.sics.kompics.p2p.peer.PeerPort;
@@ -71,12 +78,58 @@ public final class Tracker extends ComponentDefinition {
 //-------------------------------------------------------------------
 	Handler<UpdateBuffer> handleUpdateBuffer = new Handler<UpdateBuffer>() {
 		public void handle(UpdateBuffer event) {
+			//step 2
+			int pieceReceivedIndex = event.getPieceIndex();
+			PeerAddress peer = event.getPeerSource();
+			
+			Boolean[] peerBuffer = peers.get(peer);
+			peerBuffer[pieceReceivedIndex] = true;
 		}
 	};
 
 //-------------------------------------------------------------------
 	Handler<GetUploaderRequest> handleGetUploader = new Handler<GetUploaderRequest>() {
 		public void handle(GetUploaderRequest event) {
+			boolean sent = false;
+			
+			PeerAddress requester = event.getPeerSource(); 
+			Boolean[] requesterBuffer = peers.get(requester);
+			
+			List<PeerAddress> peerSet = new ArrayList<PeerAddress>(peers.keySet()); 
+			Collections.shuffle((List<PeerAddress>) peerSet);
+
+			for (PeerAddress responder : peerSet) {
+				if (!requester.equals(responder)) {
+					Boolean[] responderBuffer = peers.get(responder);
+					
+					ArrayList<Integer> shuffledList = createRandomListOfIntegers(numOfPieces);
+					Iterator<Integer> shuffledIterator = shuffledList.iterator();
+					while (shuffledIterator.hasNext()) {
+						Integer index = (Integer) shuffledIterator.next();
+						if ((!requesterBuffer[index]) && responderBuffer[index]) {
+							//it is ok! use this
+							trigger(new GetUploaderResponse(peerSelf, requester, responder, index), network);
+							sent = true;
+							break;
+						}
+					}
+					if (sent) {
+						break;
+					}
+				}
+			}
 		}
 	};
+	
+	ArrayList<Integer> createRandomListOfIntegers(int size) {
+		ArrayList<Integer> intList = new ArrayList<Integer>();
+		for (int i = 0; i < size; i++) {
+			intList.add(new Integer(i));
+		}
+		
+		Collections.shuffle(intList);
+		
+		return intList;
+	}
 }
+
